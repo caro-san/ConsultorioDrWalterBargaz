fea472c07f0cc66c74799f6e399c1eb859b91294
unknown <jeyca0115@gmail.com>
2020-07-25 00:42:51 -0300

    Cambios en pagina principal y servicios

--- js/script_banner.js
+++ js/script_banner.js
@@ -1,15 +1,11 @@
 /* Script navbar */
 
-$(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop()>86){
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 86) {
             $('#banner-sh').addClass("shrink");
-        } else{
+        } else {
             $("#banner-sh").removeClass("shrink");
         }
     });
 });
-
-
-
-
